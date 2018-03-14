a7048a7692f0121d924f23a2c62ac15a2d81709f
dayakar1979 <37309056+dayakar1979@users.noreply.github.com>
2018-03-14 15:46:12 -0400

    ER_Componet_nUnit

---
+++ ElectronicRatingCalculatorComponent.Tests/ERCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElectronicRatingCalculatorComponent;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ERCalculator
+    {
+        [TestMethod]
+        public void CalculateTest()
+        {
+            IOhmValueCalculator calculatorClient = new OhmValueCalculator();
+
+            var actualResult = calculatorClient.CalculateOhmValue("Red", "Black", "Red", "Red");
+
+            Assert.AreEqual(new Tuple<double, double>(2000, 2), actualResult, "CalculateTest");
+
+            actualResult = calculatorClient.CalculateOhmValue("Silver", "Silver", "Silver", "Silver");
+
+            Assert.AreEqual(new Tuple<double, double>(-0.33, 10), actualResult, "CalculateTest");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullExceptionTest()
+        {
+            var expectedParamName = "bandAColor";
+            try
+            {
+                IOhmValueCalculator calculatorClient = new OhmValueCalculator();
+                calculatorClient.CalculateOhmValue(null, "Silver", "Silver", "Silver");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual(expectedParamName, ex.ParamName);
+            }
+
+            
+        }
+    }
+
+
+}
